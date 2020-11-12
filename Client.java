af4faa222e36c290804fa7ae1b286d7d93289633
Zygen <Xsaber64@Gmail.com>
2020-11-12 17:12:41 -0600

    Created functions for the Client class, minus the main function

--- Client.java
+++ Client.java
@@ -1,7 +1,56 @@
 // Imports
 import java.io.*;  
 import java.net.*;  
-public class Client {  
+
+// * Class for client
+public class Client {
+    // Params
+    private String hostname;
+    private int portNum;
+    private String username;
+
+    // * Primary Constructor for Client class
+    public Client(String hostname, int portNum){
+        this.hostname = hostname;
+        this.portNum = portNum;
+    }
+
+    // * Execute the client
+    public void execute() {
+        try { 
+            Socket socket = new Socket(hostname, portNum);
+
+            System.out.println("Connected to the chat server successfully!");
+
+            // Start a thread to read input
+            new ReadThread(socket, this).start();
+
+            // Start a thread to write input
+            new WriteThread(socket, this).start();
+        
+        // ! Can't find the server
+        } catch (UnknownHostException e) {
+            System.out.println("The server specified was not found: " + e.getMessage());
+
+        // ! Input error
+        } catch (IOException e) {
+            System.out.println("Input Error: " + e.getMessage());
+        }
+
+    }
+
+    // * setter for the username parameter for the client
+    void setUsername(String username) {
+        this.username = username;
+    }
+
+    // * getter for the username parameter for the client
+    String getUsername() {
+        return this.username;
+    }
+
+    
+// * Main function to operate client initialization and chatting 
 public static void main(String[] args) {  
     try{      
         Socket myClientSocket = new Socket("localhost", 6666);  
